2c28e9c6b7ee5f41195a3bf60daaff8d283c3f74
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-21 11:59:15 +0530

    class component

---
+++ src/UseState/classCount.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+    // we can bind the functions here beause of without binding the function it can't increase the count
+    this.Increament = this.Increament.bind(this);
+  }
+  Increament() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  render() {
+    return (
+      <>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.Increament}>Increment</button>
+      </>
+    );
+  }
+}
