6737566546b8854212a88495110a02d859c502e8
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-21 14:54:25 +0530

    form data

--- src/UseState/Form.jsx
+++ src/UseState/Form.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  let inputData = { name: "", age: 0, city: "", state: "" };
+  const [input, setInput] = useState(inputData);
 
-export default Form
+  const FormSubmit = (event) => {
+    event.preventdefault();
+  };
+
+  const UpdateForm = (event) =>{
+    setInput({[event.target.name] : event.target.value})
+  }
+
+  return (
+    <>
+      <form action="" onSubmit={FormSubmit}>
+        User Name: - <input type="text" name="name" value={input.state} onChange={UpdateForm} />
+      </form>
+    </>
+  );
+};
+
+export default Form;
