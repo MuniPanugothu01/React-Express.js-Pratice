79ea9ca979be9dd996e5103d6326a2e618685b70
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-21 10:39:13 +0530

    counter

--- src/UseState/Counter.jsx
+++ src/UseState/Counter.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+    const [state,setState] = useState(0)
+    increament(){
+        setState(state + 1)
+    }
   return (
     <>
-      
+ <h1>Counter:</h1>
+ <button onClick={increament}>Increament!</button>     
     </>
   )
 }
