07b4c4ba66608f6c0a5c395c98d95c862f7e4ab0
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-22 07:26:31 +0530

    username validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Counter1 from "./UseState/classCount";
 import Counter from "./UseState/Counter";
 import Form from "./UseState/Form";
 import Profile from "./UseState/object_form";
+import UserName from './UseState/UserName'
 function App() {
   return (
     <>
       {/* <Counter /> */}
       {/* <Counter1/> */}
-      <Form />
+      {/* <Form /> */}
       {/* <Profile /> */}
+
+      <UserName/>
     </>
   );
 }
---
+++ src/UseState/UserName.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const UserName = () => {
+  const [userName, setUserName] = useState("");
+  // usename is valid or not it will check here this usestate
+  const [errorName, setErrorName] = useState("");
+  return (
+  <>
+  </>
+  );
+};
+
+export default UserName;
