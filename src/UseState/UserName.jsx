3d76b6ca924c2739e24d8df7a911ad75afcd0034
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-22 07:32:00 +0530

    validate

--- src/UseState/UserName.jsx
+++ src/UseState/UserName.jsx
@@ -4,9 +4,29 @@ const UserName = () => {
   const [userName, setUserName] = useState("");
   // usename is valid or not it will check here this usestate
   const [errorName, setErrorName] = useState("");
+
+  //   username update
+  const UserNameUpdate = (event) => {
+    setUserName(...userName, event.target.value);
+
+    const ErrorValidate = (event) =>{
+        if(userName === "" || userName === null){
+            setErrorName("userName can't be blank!")
+        }
+    }
+  };
   return (
-  <>
-  </>
+    <>
+      <label htmlFor="username">UserName</label>
+      <input
+        type="text"
+        name="username"
+        value={userName}
+        onChange={UserNameUpdate}
+      />
+
+      <button onClick={ErrorValidate}>Validateusername</button>
+    </>
   );
 };
 
