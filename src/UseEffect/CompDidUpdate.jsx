b4c564cadcd1e3fe758db2518a8adf123628522c
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-24 15:00:49 +0530

    increment

--- src/UseEffect/CompDidUpdate.jsx
+++ src/UseEffect/CompDidUpdate.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const CompDidUpdate = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     setTimeout(() => {
