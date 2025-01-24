44027d5c458e1d3c0b0103f32f888f96598a603e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-24 06:30:59 +0530

    regular expression

--- src/UseState/Password.jsx
+++ src/UseState/Password.jsx
@@ -15,14 +15,15 @@ const Password = () => {
     setConfirmPassword(event.target.value);
   };
   const ValidPassword = (event) => {
-    const regularExpression =
-      /^(?=.*\d)(?=(.\W){2})(?=.*[a-zA-Z])(?!.*\s).{1,8}$/;
+    const regularExpression = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^\w\s]).{5,8}$/;
     if (password === "" || password === null) {
       setErrorPassword("please enter the valid here!");
-    } else if (password.length < 5) {
+    } else if (password.length < 5 || password.length > 8) {
       setErrorPassword(`please ${password} enter the more than 3 character!`);
-    } else if (regularExpression.test(password)) {
-      setErrorPassword(`${password} `);
+    } else if (!regularExpression.test(password)) {
+      setErrorPassword(
+        `${password} Password must contain at least one letter, one digit, and one special character! `
+      );
     } else {
       setErrorPassword(`${password} is valid!`);
     }
