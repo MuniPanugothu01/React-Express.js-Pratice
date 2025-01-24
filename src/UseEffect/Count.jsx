e095d63e89f3dcd14c90135b3b45e1c8f416cb90
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-24 14:41:20 +0530

    text

--- src/UseEffect/Count.jsx
+++ src/UseEffect/Count.jsx
@@ -11,6 +11,7 @@ const Count = () => {
 
   return (
     <>
+    <h1>count is countinously increasing beacuse of there is no depency array then the count will updating the countinously!</h1>
       <h1>count is: {count}</h1>
     </>
   );
