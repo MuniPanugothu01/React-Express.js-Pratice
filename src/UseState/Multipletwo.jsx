9689a724111a040b90cd49fc26e5f6d5202b892b
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-25 12:23:52 +0530

    removed 0 from useState

--- src/UseState/Multipletwo.jsx
+++ src/UseState/Multipletwo.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 function Multiply() {
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
   const [result, setResult] = useState(null);
 
   const handleAddition = () => setResult(Number(num1) + Number(num2));
