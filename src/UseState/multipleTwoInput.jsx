b0e78dcf1fc4d9cdb2290c248018aabb461ec753
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-27 11:04:44 +0530

    useState

--- src/UseState/multipleTwoInput.jsx
+++ src/UseState/multipleTwoInput.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const multipleTwoInput = () => {
+    const [num1,setNum1] = useState()
+    const [nu2,setNum2] = useState()
+    
+    // 
   return (
     <>
       
