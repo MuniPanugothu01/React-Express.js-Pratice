7523d4e049933d1790db2d725c874c1ebbabd1d5
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-27 14:33:56 +0530

    switch case operation

--- src/UseEffect/SwitchCase.jsx
+++ src/UseEffect/SwitchCase.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SwitchCase = () => {
     const [num1, setNum1] = useState();
     const [num2, setNum2] = useState();
     const [result, setResult] = useState();
+
+    // operation the buttons which button we are using to click the button
+    const [operation, setOperation] = useState(null);
+
+    useEffect(()=>{
+if(operation){
+    let Number1 = Number(num1);
+    let Numbre2 = Number(num2);
+}
+    },operation,num1,num2)
   return (
     <>
       
