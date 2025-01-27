e7eba32ee8008a00adb94cfb133e07c739c83a60
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-27 14:38:34 +0530

    operation

--- src/UseEffect/SwitchCase.jsx
+++ src/UseEffect/SwitchCase.jsx
@@ -1,24 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const SwitchCase = () => {
-    const [num1, setNum1] = useState();
-    const [num2, setNum2] = useState();
-    const [result, setResult] = useState();
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [result, setResult] = useState();
 
-    // operation the buttons which button we are using to click the button
-    const [operation, setOperation] = useState(null);
+  // operation the buttons which button we are using to click the button
+  const [operation, setOperation] = useState(null);
 
-    useEffect(()=>{
-if(operation){
-    let Number1 = Number(num1);
-    let Numbre2 = Number(num2);
-}
-    },operation,num1,num2)
+  const handleInput = (event) => {
+    setNum1(event.target.value);
+  };
+
+  const handleInput2 = (event) => {
+    setNum2(event.target.value);
+  };
+
+  useEffect(
+    () => {
+      if (operation) {
+        let Number1 = Number(num1);
+        let Numbre2 = Number(num2);
+        switch (operation) {
+          case "add":
+            setResult(Number1 + Numbre2);
+        }
+      }
+    },
+    operation,
+    num1,
+    num2
+  );
   return (
     <>
-      
+      Enter the Number1:{" "}
+      <input type="number" name="add" onChange={handleInput} />
+      Enter the Number2: <input type="number" onChange={handleInput2} />
+      <p>result is!{result}</p>
     </>
-  )
-}
+  );
+};
 
 export default SwitchCase;
