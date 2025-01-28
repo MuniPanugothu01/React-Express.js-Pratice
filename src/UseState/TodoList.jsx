ad607e952f6569c080619a9619fca4a20016e135
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-28 07:32:47 +0530

    code

--- src/UseState/TodoList.jsx
+++ src/UseState/TodoList.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
+    const [input, setinput] = useState(0);
+    // to store the values while entering in input box
+    const [store, setStore] = useState([])
+
+    const handleInput =(event) =>{
+        setinput(event.target.value)
+    }
+    const Submit = (event) =>{
+        event.preventDefalut();
+        setStore([...store,input])
+    }
   return (
     <>
-      
+      <form action=""  onSubmit={Submit}>
+        <input type="text" onChange={handleInput} value={input} />
+        <input type="submit" />
+      </form>
     </>
   )
 }
