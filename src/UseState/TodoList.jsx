452e9944d44d8ce075f5348628b23b35a63a31e9
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-28 07:43:41 +0530

    dd

--- src/UseState/TodoList.jsx
+++ src/UseState/TodoList.jsx
@@ -1,40 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const TodoList = () => {
-    const [input, setinput] = useState(0);
-    // to store the values while entering in input box
-    const [store, setStore] = useState([])
+  const [input, setinput] = useState("");
+  // to store the values while entering in input box
+  const [store, setStore] = useState([]);
 
-    const handleInput =(event) =>{
-        setinput(event.target.value)
-    }
-    const Submit = (event) =>{
-        event.preventDefault();
-        setStore([...store,input])
-    }
+  const handleInput = (event) => {
+    setinput(event.target.value);
+  };
+  const Submit = (event) => {
+    event.preventDefault();
+    setStore([...store, input]);
+  };
 
-    {
-        var data = store.map((item,index)=>{
-            return(
-                <>
-                <li key={index}>{item}</li>
-                </>
-            )
-        })
-    }
+  {
+    var data = store.map((item, index) => {
+      return (
+        <>
+          <li key={index}>
+            {item} <button>delete</button>
+            <button>Edit</button>{" "}
+          </li>
+        </>
+      );
+    });
+  }
   return (
     <>
-      <form action=""  onSubmit={Submit}>
-        <input type="text" onChange={handleInput} value={input} />
-        <input type="submit" value='register' />
+      <form action="" onSubmit={Submit}>
+        <input
+          type="text"
+          onChange={handleInput}
+          value={input}
+          placeholder="enter the character"
+        />
+        <input type="submit" value="register" />
       </form>
-      
-        <ul>
-            {data}
-        </ul>
-      
+
+      <ul>{data}</ul>
     </>
-  )
-}
+  );
+};
 
 export default TodoList;
