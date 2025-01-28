7bee1918373ec37dab370f615c8d70ca7d662188
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-28 06:10:28 +0530

    Switch case Operation

--- src/UseEffect/SwitchCase.jsx
+++ src/UseEffect/SwitchCase.jsx
@@ -16,35 +16,48 @@ const SwitchCase = () => {
     setNum2(event.target.value);
   };
 
-  const handleOperation = (event) =>{
-    let OperatonType = event.target.name
+  const handleOperation = (event) => {
+    let OperatonType = event.target.name;
     setOperation(OperatonType);
-  }
+  };
 
-  useEffect(
-    () => {
-      if (operation) {
-        let Number1 = Number(num1);
-        let Numbre2 = Number(num2);
-        switch (operation) {
-          case "add":
-            setResult(Number1 + Numbre2);
-        }
+  useEffect(() => {
+    if (operation) {
+      let Number1 = Number(num1);
+      let Numbre2 = Number(num2);
+      switch (operation) {
+        case "add":
+          setResult(Number1 + Numbre2);
+          break;
+        case "sub":
+          setResult(Number1 - Numbre2);
+          break;
+        case "mul":
+          setResult(Number1 * Numbre2);
+          break;
+        case "div":
+          setResult(Number1 % Numbre2);
+          break;
       }
-    },
-    [operation,
-    num1,
-    num2]
-  );
+    }
+  }, [operation, num1, num2]);
   return (
     <>
       Enter the Number1:{" "}
       <input type="number" name="add" onChange={handleInput} />
       Enter the Number2: <input type="number" onChange={handleInput2} />
-      <button name="add" onClick={handleOperation}>+</button>
-      <button name="sub" onClick={handleOperation} >-</button>
-      <button name="mul" onClick={handleOperation} >*</button>
-      <button name="div" onClick={handleOperation}>%</button>
+      <button name="add" onClick={handleOperation}>
+        +
+      </button>
+      <button name="sub" onClick={handleOperation}>
+        -
+      </button>
+      <button name="mul" onClick={handleOperation}>
+        *
+      </button>
+      <button name="div" onClick={handleOperation}>
+        %
+      </button>
       <p>result is!{result}</p>
     </>
   );
