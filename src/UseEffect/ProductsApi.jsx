0166e3f4297b37e0ae15584fb5170e90a1a0af97
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-30 07:20:46 +0530

    installed the react-router-dom

--- src/UseEffect/ProductsApi.jsx
+++ src/UseEffect/ProductsApi.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ProductsApi = () => {
+  const [store, setStore] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      console.log(response);
+      setStore(response.data);
+    });
+  }, []);
+
+  // map method to use the dispaly the products
+  let Data = store.map((val, index) => {
+    return (
+      <tr key={index}>
+        <td>{val.id}</td>
+        <td>{val.title}</td>
+        <td>{val.price}</td>
+        <td>
+          <button>SeeMore</button>
+        </td>
+      </tr>
+    );
+  });
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h1>FakeStore Data!</h1>
+      <table>
+        <thead>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Button</th>
+        </thead>
+        <tbody>{Data}</tbody>
+      </table>
+    </>
+  );
+};
 
 export default ProductsApi;
