1ab6e0ad2fd2850285372de3bf74cb4797d1c331
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-03 14:42:07 +0530

    useState

--- src/UseState/ThemeColor.jsx
+++ src/UseState/ThemeColor.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ThemeColor = () => {
-    const [color, setColor] = useState('white');
+  const [color, setColor] = useState("white");
 
+  // handleTheme function
+  const handleTheme = () => {
+    if (color === "white") {
+      setColor("black");
+      document.body.style.backgroundColor = "black";
+    } else if (color === "black") {
+      setColor("red");
+      document.body.style.backgroundColor = "red";
+    } else {
+      setColor("white");
+      document.body.style.backgroundColor = "white";
+    }
+  };
 
   return (
     <>
-  <p>Theme color is </p>
-
+      <p>Theme color is {color} </p>
+      <button onClick={handleTheme}>Theme</button>
     </>
-  )
-}
+  );
+};
 
-export default ThemeColor
+export default ThemeColor;
