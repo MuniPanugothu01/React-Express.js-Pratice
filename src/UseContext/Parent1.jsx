b2e4facfe17b8af18a3a7ff107e0910dcd329dc7
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-03 11:20:52 +0530

    code

---
+++ src/UseContext/Parent1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parent1 = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Parent1;
---
+++ src/UseContext/child1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const child1 = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default child1
