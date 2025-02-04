a2c145591a8f3219e361808c1077419eb7241baa
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-04 11:05:59 +0530

    style adding the context

--- src/InfiniteScroll/InfiniteScrolling.jsx
+++ src/InfiniteScroll/InfiniteScrolling.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-
+import "./style.css";
 const InfiniteScrolling = () => {
   const [dataScroll, setDataScroll] = useState(Array.from({ length: 20 }));
 
-const ScrollNext = () =>{
-  setDataScroll()
-}
-
+  const ScrollNext = () => {
+    setDataScroll();
+  };
 
   return (
     <>
       <InfiniteScroll dataLength={dataScroll.length} next={ScrollNext}>
         {dataScroll.map((item, index) => {
           return (
-            <div className="div1Scrolling">this is a div number is {index} times repeates it.</div>
+            <div className="div1Scrolling">
+              this is a div number is {index} times repeates it.
+            </div>
           );
         })}
       </InfiniteScroll>
