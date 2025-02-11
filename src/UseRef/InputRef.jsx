8cc1b86dfeacdb0f26d6706048b2bf223eeff931
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-11 12:03:54 +0530

    added the button when ever use click the button in empty button then it blinks to input filed

--- src/UseRef/InputRef.jsx
+++ src/UseRef/InputRef.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useRef } from "react";
 
 const InputRef = () => {
+  const Input = useRef("");
+
+  const handleInput = () => {
+    Input.current.focus();
+  };
   return (
     <>
-      
+      <input type="text" ref={Input} placeholder="enter the name" />
+      <button onClick={handleInput}>Click</button>
     </>
-  )
-}
+  );
+};
 
-export default InputRef
+export default InputRef;
