8c4e0b19ab49cf029d3c976a44455f0d434d347e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-11 12:45:28 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -38,17 +38,9 @@ import InfiniteScrolling from "./InfiniteScroll/InfiniteScrolling.jsx";
 
 // useRef importiong files
 import FocusInput from "./UseRef/useRef1.jsx";
-import CountRef from './UseRef/CountRef.jsx';
-import InputRef from './UseRef/InputRef.jsx';
-import UseEffectInput from './UseRef/UseEffectInput.jsx'
-
-
-
-
-
-
-
-
+import CountRef from "./UseRef/CountRef.jsx";
+import InputRef from "./UseRef/InputRef.jsx";
+import UseEffectInput from "./UseRef/UseEffectInput.jsx";
 
 function App() {
   return (
@@ -105,8 +97,8 @@ function App() {
       {/* UseRef Component */}
       {/* <FocusInput /> */}
       {/* <CountRef/> */}
-    {/* <InputRef/> */}
-    <UseEffectInput/>
+      {/* <InputRef/> */}
+      <UseEffectInput />
     </>
   );
 }
