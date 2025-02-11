9a67ba93bdf09eff985411ecd1a6cf1f072e0845
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-11 12:48:02 +0530

    while using UseEffect

--- src/UseRef/UseEffectInput.jsx
+++ src/UseRef/UseEffectInput.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 const UseEffectInput = () => {
+  const Input = useRef("");
+  useEffect(() => {
+    Input.current.focus();
+  }, []);
   return (
     <>
       <h1>input field component using the useEffect!</h1>
+      <input type="text" ref={Input} placeholder="enter the name!" />
+      <button>Click</button>
     </>
-  )
-}
+  );
+};
 
 export default UseEffectInput;
