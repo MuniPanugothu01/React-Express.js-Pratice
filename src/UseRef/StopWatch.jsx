5c8565ee314f78401c86e620793175c947d4f129
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-12 11:34:11 +0530

    text

--- src/UseRef/StopWatch.jsx
+++ src/UseRef/StopWatch.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const StopWatch = () => {
   return (
     <>
-      
+      <h1>Stop Watch while using the UseRef()</h1>
     </>
-  )
-}
+  );
+};
 
 export default StopWatch;
