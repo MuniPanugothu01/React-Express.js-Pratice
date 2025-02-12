0cb0a5135a6b73a66e318e4c14888712200ecb7b
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-12 11:52:24 +0530

    code for stop timer

--- src/UseRef/StopWatch.jsx
+++ src/UseRef/StopWatch.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const StopWatch = () => {
+  const [count, setCount] = useState(0);
+  const TimerValue = useRef();
+
+  const handleStartTimer = () => {
+    TimerValue.current = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+  };
+  const handleStopTimer = () => {
+    clearInterval(TimerValue.current);
+  };
+
   return (
     <>
+      <h1>{count}</h1>
+      <button onClick={handleStartTimer}>Start</button>
+      <button onClick={handleStopTimer}>Stop</button>
       <h1>Stop Watch while using the UseRef()</h1>
     </>
   );
