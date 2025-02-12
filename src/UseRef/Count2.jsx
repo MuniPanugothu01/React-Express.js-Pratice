990c4d1e0fb7a7031ec288ccd3a2105fba13fd0a
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-12 10:56:01 +0530

    code of value increment

--- src/UseRef/Count2.jsx
+++ src/UseRef/Count2.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useRef } from "react";
 
 const Count2 = () => {
+  const CountRef = useRef(0);
+  const handleIncrement = () => {
+    CountRef.current++;
+    console.log("Count value is increment!", CountRef.current++);
+  };
   return (
     <>
       <h1>Count Increment! but it won't re-render the component</h1>
+      <button onClick={handleIncrement}>Click</button>
     </>
-  )
-}
+  );
+};
 
-export default Count2
+export default Count2;
