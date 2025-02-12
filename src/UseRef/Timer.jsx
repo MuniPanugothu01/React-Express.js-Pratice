61190f54a46d1a93f5f846f0638ef3572e28863b
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-12 12:32:59 +0530

    created the useState

--- src/UseRef/Timer.jsx
+++ src/UseRef/Timer.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useReducer, useRef, useState } from 'react'
 
 const Timer = () => {
+    const [ time, setTime] = useState({hours:0,minutes:0,seconds:0})
+    // creating the useRef()
+    const preValue = useRef(null);
   return (
     <>
       
