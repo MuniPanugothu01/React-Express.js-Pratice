3d6fcb8cc23c1943cb711cc011bfcac3462a297a
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-17 00:36:32 +0530

    displaying the data

--- src/UseReducer/Crud.jsx
+++ src/UseReducer/Crud.jsx
@@ -1,28 +1,53 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer, useState } from "react";
 
 // InitialState to take as the empty array = []
 const InitialState = [];
 
-const Crud = () => {
-    const [inpupt, setInput] = useState('');
+// reducer function()
 
-// useReducer() function, reducer can return the array
-const [state, dispatch] = useReducer(reducer, InitialState);
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, { id: Date.now(), inpupt: action.playload }];
+     
 
-
-// handleInputval function
-const handleInputval = (event) =>{
-    setInput(event.target.value);
+    default:
+      break;
+  }
 }
+
+const Crud = () => {
+  const [inpupt, setInput] = useState("");
+
+  // useReducer() function, reducer can return the array
+  const [state, dispatch] = useReducer(reducer, InitialState);
+
+  // handleInputval function
+  const handleInputval = (event) => {
+    setInput(event.target.value);
+  };
 
   return (
     <>
       <h1>TotoList with Crud Operation!</h1>
-      <div style={{textAlign:'center'}}>
-        <input type="text" placeholder='enter the text' value={inpupt} onChange={handleInputval} />
+      <div style={{ textAlign: "center" }}>
+        <input
+          type="text"
+          placeholder="enter the text"
+          value={inpupt}
+          onChange={handleInputval}
+        />
+
+        <button
+          onClick={() => {
+            dispatch({ type: "ADD" });
+          }}
+        >
+          Add
+        </button>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Crud;
