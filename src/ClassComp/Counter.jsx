8970a707d96392ae0b44b3f2acc59ab06c73a2b9
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-01 10:03:01 +0530

    bind method, bind is used to bind the this key word current functionality

--- src/ClassComp/Counter.jsx
+++ src/ClassComp/Counter.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
 
-class Counter extends React.Component{
-    constructor(){
-        super();
-        this.state = { count : 0}
-    }
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+    this.UpdateCount = this.UpdateCount.bind(this);
+  }
+  UpdateCount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.UpdateCount}>Increment</button>
+      </>
+    );
+  }
 }
 
-export default Counter
+export default Counter;
