69944468f6414a654a22f12f9162d7297657acc4
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-10 13:05:09 +0530

    email validation is completed

--- src/UseState/EmailValid.jsx
+++ src/UseState/EmailValid.jsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const EmailValid = () => {
+  const [email, setEmail] = useState("");
+  // error display useState
+  const [error, setError] = useState("");
 
-const [email, setEmail] = useState('')
-// error display useState
-const [error, setError] = useState('')
+  const HandleMail = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const HandleMail  = (event) =>{
-setEmail(event.target.value);
+  const HandleValid = (event) => {
+    const RegMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email === "" || email === null) {
+      setError("mail cant be empty!");
+    } else if (email.length < 3) {
+      setError("email more than 3 characters!");
+    } else if (!RegMail.test(email)) {
+      setError("email atleast special character!");
+    } else {
+      setError("");
     }
-
-    const HandleValid = (event) =>{
-        const RegMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if(email === '' || email === null){
-            setError('mail cant be empty!')
-        }
-        else if(email.length < 3){
-            setError('email more than 3 characters!')
-        }
-        else if(RegMail.test(email)){
-            setError('email atleast special character!')
-        }
-    }
+  };
   return (
     <>
       <h1>Email Validate</h1>
       <label htmlFor="email">Email:</label>
-      <input type="email" name='email' value={email} onChange={HandleMail}/>
+      <input type="email" name="email" value={email} onChange={HandleMail} />
       <button onClick={HandleValid}>ValidEmail</button>
+      <br />
 
-      {error && <span style={{color:'red'}}>{error}</span> }
+      {error && <span style={{ color: "red" }}>{error}</span>}
+
+      <h5>
+        Your Mail is:<span style={{ color: "green" }}>{email}</span>
+      </h5>
     </>
-  )
-}
+  );
+};
 
 export default EmailValid;
