c701e7ed1ba4d4cd78815a9ed31da73115afc9de
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-14 11:56:45 +0530

    count value update insdie the button i took the useEffect

--- src/UseEffect/ValueCount.jsx
+++ src/UseEffect/ValueCount.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const ValueCount = () => {
+  const [count, setCount] = useState(0);
+
+  const HandleCount = () => {
+    useEffect(() => {
+      setCount(count + 1);
+    }, []);
+  };
+
   return (
     <>
-      
+      <h1>Count value is: {count}</h1>
+      <button onClick={HandleCount}>Click</button>
     </>
-  )
-}
+  );
+};
 
 export default ValueCount;
