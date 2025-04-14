498a930a3358b39ae6498a985c94e28874def35f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-14 11:52:30 +0530

    form validation

--- src/UseState/UserFormValid.jsx
+++ src/UseState/UserFormValid.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const UserFormValid = () => {
-// useState first name
-const [FirstName, setFirstName] = useState('');
-// first name error useState
-const [firstError, setFirstError] = useState('')
+  // useState first name
+  const [FirstName, setFirstName] = useState("");
+  // first name error useState
+  const [firstError, setFirstError] = useState("");
 
-    return (
+  return (
     <>
-<h1>Form validataion!</h1>
+      <h1>Form validataion!</h1>
     </>
-  )
-}
+  );
+};
 
 export default UserFormValid;
