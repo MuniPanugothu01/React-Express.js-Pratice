1cd387730711af4cbb7809071dc557a85863c79e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-14 15:17:40 +0530

    w

--- src/UseEffect/FakeDisplay.jsx
+++ src/UseEffect/FakeDisplay.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FakeDisplay = () => {
+const [data, setData] = useState([]);
+
+useEffect(()=>{
+fetch().then(res)
+},[])
+
   return (
     <>
       <h1>Fake Store Data</h1>
+
     </>
   )
 }
