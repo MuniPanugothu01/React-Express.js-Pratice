62bfef8bc89fe164baa64a5c272a9765595383f6
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-21 06:44:38 +0530

    Schema defined for users data

--- Server/index.jsx
+++ Server/index.jsx
@@ -1,7 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-mongoose.connect("mongodb://localhost:27017/Crud");
+mongoose
+  .connect("mongodb://localhost:27017/Crud")
+  .then(() => {
+    console.log("mongoose is connected!");
+  })
+  .catch(() => {
+    console.log("mongoose is not connected!");
+  });
+
+// usersSchema to define the users formate
+const usersSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Usermodel = mongoose.model("users", usersSchema);
+
+app.get("/getUsers", (req, res) => {
+  res.json(Usermodel.find());
+});
 
 let PORT = 3001;
 app.listen(PORT, () => {
