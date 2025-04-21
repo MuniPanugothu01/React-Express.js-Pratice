30fe1fb0b1978738b775dc6df2165f478f4660eb
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-21 06:36:24 +0530

    mongoose connected to the node js backend

--- Server/index.jsx
+++ Server/index.jsx
@@ -1,1 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+mongoose.connect("mongodb://localhost:27017/Crud");
 
+let PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`port is connected is ${PORT}`);
+});
