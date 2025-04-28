96706d1bbaa6f93703baeae77f5083f905a6d927
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-04-28 12:27:18 +0530

    files export and import modules

---
+++ Server/file1.js
@@ -0,0 +1,3 @@
+var x = 'muni';
+console.log('this is the file1');
+module.exports={x}
---
+++ Server/file2.js
@@ -0,0 +1,5 @@
+require('./file1')
+console.log('this is file 2!');
+const y = require('./file1');
+console.log(y);
+
