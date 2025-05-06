c1ada221de50f64aed33d5aab87fb4912e800ca1
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-06 13:38:55 +0530

    added the styles to cards

--- src/UseEffect/CardFakeAPI.jsx
+++ src/UseEffect/CardFakeAPI.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+// import the bootstrap
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const CardFakeAPI = () => {
   // useState to display the data
@@ -15,16 +18,25 @@ const CardFakeAPI = () => {
         setDisplay(res);
       });
   }, [display]);
-const Fakedata = display.map((item,index)=>{
-    return(
-
-    )
-})
-
+  const Fakedata = display.map((item, index) => {
+    return (
+      <Card style={{ width: "18rem" }} >
+        <Card.Body>
+          <Card.Title> <img src={item.image} alt={item.image} height={120} width={120} /> </Card.Title>
+          <Card.Text>
+          {item.description}
+          </Card.Text>
+          <Button variant="primary">Add TO Cart</Button>
+          <Button variant="primary" style={{marginLeft:'10px', textAlign:'center'}}>Buy</Button>
+        </Card.Body>
+      </Card>
+    );
+  });
 
   return (
     <>
       <h1>Shopping Site</h1>
+      <p>{Fakedata}</p>
     </>
   );
 };
