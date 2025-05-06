781f4ff9d110a2b7f5abb93ff57197a6a858fb7f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-06 13:23:29 +0530

    shopping card displayed in cards

--- src/UseEffect/CardFakeAPI.jsx
+++ src/UseEffect/CardFakeAPI.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const CardFakeAPI = () => {
+  // useState to display the data
+  const [display, setDisplay] = useState([]);
+
+  // useEffect for fetch the data
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((responase) => {
+        return responase.json();
+      })
+      .then((res) => {
+        console.log(res);
+        setDisplay(res);
+      });
+  }, [display]);
+const Fakedata = display.map((item,index)=>{
+    return(
+
+    )
+})
+
+
   return (
     <>
-      
+      <h1>Shopping Site</h1>
     </>
-  )
-}
+  );
+};
 
 export default CardFakeAPI;
