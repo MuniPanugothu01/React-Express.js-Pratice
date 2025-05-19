4e3545ec1ec222d4fa2a2ab53c68bf77661191d9
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-19 12:38:45 +0530

    obj data displayed in browser through res.write

--- Server/GitRepo/index.js
+++ Server/GitRepo/index.js
@@ -2,9 +2,16 @@ console.log("hello this is the GitRepo Flder");
 console.log("Muni");
 
 const http = require("http");
+
+const obj = {
+  name: "muni",
+  age: 23,
+  course: "mernstack",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(201, { "content-Type": "application/json" });
-  res.write("this is the first");
+  res.write(JSON.stringify(obj));
   res.end();
 });
 server.listen(3002, () => {
