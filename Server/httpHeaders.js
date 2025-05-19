adeaf813ee0852d3e894d1281efceccb795bdfae
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-19 12:51:44 +0530

    added the message code and SuccessCode to the server

--- Server/httpHeaders.js
+++ Server/httpHeaders.js
@@ -1,14 +1,24 @@
 const http = require("http");
+const SuccessCode = 200;
+const messageCode = "data rectrived successfully"
+
 const data = {
   name: "Muni",
   age: 23,
   role: "mern stack developer",
   gender: "male",
 };
+// create the object to display in browser
+const resData = {
+    "status" : SuccessCode,
+    "message" : messageCode,
+    "res" : data 
+}
+
 
 const server = http.createServer((req, res) => {
   res.writeHead(201, { "content-Type": "application/json" });
-  res.write(JSON.stringify(data));
+  res.write(JSON.stringify(resData));
   res.end();
 });
 
