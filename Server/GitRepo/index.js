5ea72aae6134cf724789a54c979edcd2ba13c15f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-19 12:30:52 +0530

    http server created along with headers added

--- Server/GitRepo/index.js
+++ Server/GitRepo/index.js
@@ -1,2 +1,12 @@
 console.log("hello this is the GitRepo Flder");
 console.log("Muni");
+
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(201, { "content-Type": "application/json" });
+  res.write("this is the first");
+  res.end();
+});
+server.listen(3002, () => {
+  console.log(`port is connected here`);
+});
