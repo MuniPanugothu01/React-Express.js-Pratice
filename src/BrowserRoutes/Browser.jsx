1892fc96208e3052253ef106c30f2befc5d7e240
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-20 11:32:00 +0530

    created the link tag and Browser routes

--- src/BrowserRoutes/Browser.jsx
+++ src/BrowserRoutes/Browser.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+// import the browser routes
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const Browser = () => {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/footer" element={<Footer />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default Browser
+export default Browser;
--- src/BrowserRoutes/Navbar.jsx
+++ src/BrowserRoutes/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from "react";
+// import the Link tag to this file
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      
+      <Link to="/"> Home </Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/footer">Footer</Link>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
