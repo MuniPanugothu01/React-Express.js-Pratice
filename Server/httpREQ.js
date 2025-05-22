6eab0d738d519496467bc7dc5a1b72aa11916393
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-22 11:14:14 +0530

    to fin the path name

--- Server/httpREQ.js
+++ Server/httpREQ.js
@@ -1,16 +1,24 @@
 const http = require("http");
-const url = require('url')
+const url = require("url");
 const Server = http.createServer((req, res) => {
-  console.log(req, "resquest object");
-  console.log(req.method,'this is methods on request');
-  console.log(req.url,'this method can show the url on server');
-// http://localhost:3001/home i want thi entire url 
-console.log(`http://${req.headers.host}${req.url}`);
+  //   console.log(req, "resquest object");
+  //   console.log(req.method,'this is methods on request');
+  //   console.log(req.url,'this method can show the url on server');
+  // http://localhost:3001/home i want thi entire url
+  // console.log(`http://${req.headers.host}${req.url}`);
 
-const FullUrl = `http://${req.headers.host}${req.url}`
-  console.log(url.parse(FullUrl)); 
-  console.log(req.headers.host);
-  console.log(req.headers);
+  const FullUrl = `http://${req.headers.host}${req.url}`;
+  //   console.log(url.parse(FullUrl));
+  //   console.log(req.headers.host);
+  //   console.log(req.headers);
+
+  //   path name this is the one way to find the path name.
+//   above one is the another way to find the path name
+  const parsed = url.parse(req.url);
+  console.log(parsed);
+  console.log(parsed.pathname);
+  
+
   res.writeHead(200, { "content-Type": "application/json" });
   res.end("end the server");
 });
