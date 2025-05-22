d331aeef8570da3b628fdf3f53ac432eb8ffe1d9
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-22 10:18:56 +0530

    req.method and req.url this is can give me methods on request and url path it will give

--- Server/httpREQ.js
+++ Server/httpREQ.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const Server = http.createServer((req, res) => {
   console.log(req, "resquest object");
+  console.log(req.method,'this is methods on request');
+  console.log(req.url,'this method can show the url on server');
   res.writeHead(200, { "content-Type": "application/json" });
   res.end("end the server");
 });
