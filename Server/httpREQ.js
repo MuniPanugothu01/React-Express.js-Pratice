ccd300f54e8621b3cc9b284f15ff9f41e4ae2caa
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-22 10:49:51 +0530

    headers in url path

--- Server/httpREQ.js
+++ Server/httpREQ.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const url = require('url')
 const Server = http.createServer((req, res) => {
   console.log(req, "resquest object");
   console.log(req.method,'this is methods on request');
   console.log(req.url,'this method can show the url on server');
+  console.log(url.parse(req.url));
+  console.log(req.headers.host);
   res.writeHead(200, { "content-Type": "application/json" });
   res.end("end the server");
 });
