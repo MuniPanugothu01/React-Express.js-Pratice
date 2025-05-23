5638864533d3df3a5f1d23c69439c858a4c7587f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-23 12:36:45 +0530

    basic routing completed with external file export and imported

--- Server/BasicRouting.js
+++ Server/BasicRouting.js
@@ -7,6 +7,8 @@ const {
   InternalCodeError,
   notFountCode,
   SuccesCode,
+  SuccessCodeInsteat,
+  SuccessCodeInsteatMessage,
 } = require("./StatusCodeBASIC");
 
 //  Userdata
@@ -14,15 +16,74 @@ const data = {
   name: "Muni",
   age: 23,
 };
+// aboutus userData
+const UserData = {
+  Name: "John",
+  age: 30,
+  role: "frontend",
+};
+
+// contactRoute Users
+const User1 = {
+  name: "user1",
+  age: 23,
+  role: "backend",
+  experience: "fresher",
+};
+const user2 = {
+  name: "user2",
+  age: 30,
+  role: "frontend",
+  experience: "fresher",
+};
+const user3 = {
+  name: "user3",
+  age: 26,
+  role: "devops",
+  experience: "mid range",
+};
+const user4 = {
+  name: "user4",
+  age: 27,
+  role: "mernStack developer",
+  experience: "fresher",
+};
+// userInfo
+const UserInfo = { User1: User1, user2: user2, user3: user3, user4: user4 };
 
 const Server = http.createServer((req, res) => {
   const Parsed = url.parse(req.url);
   console.log(Parsed);
   if (Parsed.pathname === "/") {
     res.writeHead(200, { "content-Type": "application/json" });
-    res.write(JSON.stringify([{ status: SuccesCode, message: SuccessMessage, data }]));
+    res.write(
+      JSON.stringify([
+        { status: SuccesCode, message: SuccessMessage, User: data },
+      ])
+    );
+  } else if (Parsed.pathname === "/about") {
+    res.writeHead(201, { "content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        status: SuccessCodeInsteat,
+        message: SuccessCodeInsteatMessage,
+        Users: UserData,
+      })
+    );
+  } else if (Parsed.pathname === "/contact") {
+    res.writeHead(200, { "content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        status: SuccesCode,
+        message: SuccessMessage,
+        TotalUsers: UserInfo,
+      })
+    );
+  } else {
+    res.writeHead(notFountCode);
+    res.write("user not found");
   }
-  res.end()
+  res.end();
 });
 
 let PORT = 3006;
--- Server/StatusCodeBASIC.js
+++ Server/StatusCodeBASIC.js
@@ -1,11 +1,12 @@
 const SuccesCode = 200;
 const notFountCode = 404;
 const InternalCodeError = 500;
-
+const SuccessCodeInsteat = 201;
+const SuccessCodeInsteatMessage = "data instead successfully";
 const SuccessMessage = "data rectrived Successfully";
 const NotFoundMessage = "Data not found in Server";
 const interErrorMessage = "server crashed"
 
 module.exports = {
-    SuccesCode, notFountCode, InternalCodeError, SuccessMessage, NotFoundMessage,interErrorMessage
+    SuccesCode, notFountCode, InternalCodeError, SuccessMessage, NotFoundMessage,interErrorMessage, SuccessCodeInsteat, SuccessCodeInsteatMessage
 }
