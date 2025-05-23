94b8500dc86a3834c5aca64b7326314bc7beb142
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-23 08:47:56 +0530

    Users details

--- Server/Routing.js
+++ Server/Routing.js
@@ -14,12 +14,33 @@ const Server = http.createServer((req, res) => {
   };
 
 // contact information
-const InfoUser = {
+const User1 = {
     name : 'User',
     age : 23,
     city : 'Hyd',
     Role : "FullStack developer"
 }
+const User2 = {
+    name : "user2",
+    age : 20,
+    city :'Delhi',
+    Role : "backend Developer"
+}
+const User3 = {
+    name : "user3",
+    age : 25,
+    city : "banglore",
+    Role : "Frontend Developer"
+}
+const User4 = {
+    name : 'user4',
+    age : 26,
+    city : 'Munbai',
+    Role : "devops"
+}
+
+// To display in array formate all users
+const UserInfo = [User1, User2, User3, User4]
 
   if (ParsedUrl.pathname === "/") {
     res.writeHead(200, { "content-Type": "text/plain" });
@@ -29,7 +50,7 @@ const InfoUser = {
     res.write(JSON.stringify(Data));
   } else if (ParsedUrl.pathname === "/contact") {
     res.writeHead(200, { "content-Type": "text/plain" });
-    res.write(JSON.stringify(InfoUser));
+    res.write(JSON.stringify(UserInfo));
   } else {
     res.writeHead(400, { "content-Type": "text/plain" });
     res.write("getting server error");
