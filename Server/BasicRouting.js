53fca45d82ff188d9386622d139bb5f2153e35f7
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-23 12:07:41 +0530

    home section competed basic routing

--- Server/BasicRouting.js
+++ Server/BasicRouting.js
@@ -1,11 +1,31 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const {
+  interErrorMessage,
+  NotFoundMessage,
+  SuccessMessage,
+  InternalCodeError,
+  notFountCode,
+  SuccesCode,
+} = require("./StatusCodeBASIC");
 
-const Server = http.createServer((req,res)=>{
-    const Parsed = url.parse(req.url)
-    console.log(Parsed);
-    if(Parsed.pathname === '/'){
-        res.writeHead(200,{"content-Type":"application/json"});
-        res.write()
-    }
-})
+//  Userdata
+const data = {
+  name: "Muni",
+  age: 23,
+};
+
+const Server = http.createServer((req, res) => {
+  const Parsed = url.parse(req.url);
+  console.log(Parsed);
+  if (Parsed.pathname === "/") {
+    res.writeHead(200, { "content-Type": "application/json" });
+    res.write(JSON.stringify([{ status: SuccesCode, message: SuccessMessage, data }]));
+  }
+  res.end()
+});
+
+let PORT = 3006;
+Server.listen(PORT, () => {
+  console.log(`port is connected ${PORT}!`);
+});
