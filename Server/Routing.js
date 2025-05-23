71a3812e51871d10b71d9cf7e60a5774c32a89d1
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-23 10:07:27 +0530

    status code with user data show in object type

--- Server/Routing.js
+++ Server/Routing.js
@@ -6,48 +6,56 @@ const Server = http.createServer((req, res) => {
   const ParsedUrl = url.parse(req.url);
   console.log(ParsedUrl, "here is the path");
 
+  // StatusCode
+  const StatusCode = 200;
+
   // userData
   const Data = {
     name: "Muni",
     age: 23,
-    mail: "munipanugothu2001@gmail.com",
+    role: "FrontEnd developer",
   };
 
-// contact information
-const User1 = {
-    name : 'User',
-    age : 23,
-    city : 'Hyd',
-    Role : "FullStack developer"
-}
-const User2 = {
-    name : "user2",
-    age : 20,
-    city :'Delhi',
-    Role : "backend Developer"
-}
-const User3 = {
-    name : "user3",
-    age : 25,
-    city : "banglore",
-    Role : "Frontend Developer"
-}
-const User4 = {
-    name : 'user4',
-    age : 26,
-    city : 'Munbai',
-    Role : "devops"
-}
+  const FullData = {
+    status: StatusCode,
+    data: Data,
+  };
 
-// To display in array formate all users
-const UserInfo = [User1, User2, User3, User4]
+  // contact information
+  const User1 = {
+    name: "User",
+    age: 23,
+    city: "Hyd",
+    Role: "FullStack developer",
+  };
+  const User2 = {
+    name: "user2",
+    age: 20,
+    city: "Delhi",
+    Role: "backend Developer",
+  };
+  const User3 = {
+    name: "user3",
+    age: 25,
+    city: "banglore",
+    Role: "Frontend Developer",
+  };
+  const User4 = {
+    name: "user4",
+    age: 26,
+    city: "Munbai",
+    Role: "devops",
+  };
+
+  // To display in array formate all users
+  const UserInfo = [User1, User2, User3, User4];
 
   if (ParsedUrl.pathname === "/") {
     res.writeHead(200, { "content-Type": "text/plain" });
     res.write("well come to home page");
   } else if (ParsedUrl.pathname === "/about") {
     res.writeHead(201, { "content-Type": "application/json" });
-    res.write(JSON.stringify(Data));
+    res.write(JSON.stringify(FullData));
   } else if (ParsedUrl.pathname === "/contact") {
     res.writeHead(200, { "content-Type": "text/plain" });
     res.write(JSON.stringify(UserInfo));
