a66dd135c8afc2427a17f47a7d395f9604a47ee1
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-29 22:43:55 +0530

    middle ware using query paramas

---
+++ Server/Express/QueryParm2.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+app.use((req,res,next)=>{
+    if(req.query.name){
+        next()
+    }
+    else{
+        res.status(500).send('server is not responding')
+    }
+})
+
+app.get('/',(req,res)=>{
+    console.log(req.query);
+    let name = req.query.name
+    let city = req.query.city
+    let role = req.query.role
+
+    let UserInfo = {
+        name : name,
+        city : city,
+        role : role,
+        city : city
+    }
+    res.send({status:200,"UserInfo":UserInfo})
+})
+
+let PORT = 3003;
+app.listen(PORT,()=>{
+    console.log(`port is connected ${PORT}`);
+    
+})
