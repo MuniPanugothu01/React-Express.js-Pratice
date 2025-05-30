7e2cf608c2269f3ead057d5da5c660393426350d
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-30 12:25:31 +0530

    middlewares

--- Server/Express/Middlewares.js
+++ Server/Express/Middlewares.js
@@ -28,6 +28,30 @@ app.post("/home", (req, res) => {
   res.send("home page demo");
 });
 
+// middleware for user Routings
+app.use('/user',(req,res,next)=>{
+    if(req.query.user === 'user'){
+        next()
+    }
+    else{
+        res.status(401).send('user unauthorized please go to login page!')
+    }
+})
+
+app.post('/user/home',(req,res)=>{
+    res.send('wellcome user to home page')
+})
+app.post('/user/about',(req,res)=>{
+    res.send('wellcome to user to about page')
+})
+app.post('/user/contact',(req,res)=>{
+    res.send('wellcome user to contact page')
+})
+app.post('/user/products',(req,res)=>{
+    res.send('wellcome user to products page')
+})
+
+
 let PORT = 3006;
 app.listen(PORT, () => {
   console.log(`port is connected ${PORT} here`);
