31d8559aee51119b17f896eeb43b5d0d95166fce
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-30 15:09:09 +0530

    error handling

--- Server/Express/ErrorHandle.js
+++ Server/Express/ErrorHandle.js
@@ -1,3 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.get("/home", (req, res) => {
+  throw new Error("error occures here");
+});
+// routing for admin side
+app.use("/admin", (req, res, next) => {
+  if (req.query.user) {
+    next();
+  } else {
+    res.status(401).send("server is not responding here!");
+  }
+});
+
+// routers for admin side
+app.get("/admin/home", (req, res) => {
+  let name = req.query.user;
+  console.log("user name:", name);
+  res.send("wellcome admin home page");
+});
+app.get("/admin/about", (req, res) => {
+  res.send("wellcome admin to about page");
+});
+
+// error handling router, it will act like universal router for all above routers if error can find above routers then it will be display.
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status(500).send("something went wrong in server!");
+//   }
+// });
+let PORT = 3007;
+app.listen(PORT, () => {
+  console.log(`port is connected ${PORT} here`);
+});
