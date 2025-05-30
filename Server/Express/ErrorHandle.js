1cdf82b4d043e57bf47d3fd9ac5d55f5d06af692
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-30 15:14:04 +0530

    query params input value to diaply in brower with the help of query params

--- Server/Express/ErrorHandle.js
+++ Server/Express/ErrorHandle.js
@@ -16,19 +16,25 @@ app.use("/admin", (req, res, next) => {
 // routers for admin side
 app.get("/admin/home", (req, res) => {
   let name = req.query.user;
+  let city = req.query.city;
+  console.log("city name is!", city);
   console.log("user name:", name);
-  res.send("wellcome admin home page");
+  let InputData = {
+    name: name,
+    city: city,
+  };
+  res.send({ status: 200, userData: InputData });
 });
 app.get("/admin/about", (req, res) => {
   res.send("wellcome admin to about page");
 });
 
 // error handling router, it will act like universal router for all above routers if error can find above routers then it will be display.
-// app.use("/", (err, req, res, next) => {
-//   if (err) {
-//     res.status(500).send("something went wrong in server!");
-//   }
-// });
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong in server!");
+  }
+});
 let PORT = 3007;
 app.listen(PORT, () => {
   console.log(`port is connected ${PORT} here`);
