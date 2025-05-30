68e7bd6caf5df6b33c34a2b328d225d173b49dc8
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-30 06:35:30 +0530

    home section wrote the seperate middle ware

--- Server/Express/SeprateMiddle.js
+++ Server/Express/SeprateMiddle.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const app = express()
+const e = require("express");
+const express = require("express");
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('started')
-})
+app.get("/", (req, res) => {
+  res.send("started");
+});
+
+// middle ware for get home section
+const Home = (req, res, next) => {
+  if (req.query.name) {
+    if (/^[a-zA-Z]+$/.test(req.query.name)) {
+      next();
+    } else {
+      res.status(400).send("numeric and special character is not allowed");
+    }
+  } else {
+    res.status(500).send("server is not responding");
+  }
+};
+
 const PORT = 3005;
-app.listen(PORT,()=>{
-    console.log(`port is connected ${PORT} here`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`port is connected ${PORT} here`);
+});
