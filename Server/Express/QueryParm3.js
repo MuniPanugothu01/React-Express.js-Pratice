b6f46f9d0176a902a4293d8ea0fdaf35388efa35
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-05-30 06:26:57 +0530

    in middle ware checks the request in inuput it accepts only the string not alpha bets and any special characters

--- Server/Express/QueryParm3.js
+++ Server/Express/QueryParm3.js
@@ -4,17 +4,28 @@ let app = express();
 
 app.use((req, res, next) => {
   if (req.query.name) {
-    if (/^[a-zA-Z]+$/.text(req.query.name)) {
+    if (/^[a-zA-Z]+$/.test(req.query.name)) {
       next();
     } else {
-      res.status(500).send("it requires only alpha bets");
+      res.status(400).send("it requires only alpha bets");
     }
   } else {
     res.status(500).send("name is reqired here");
   }
 });
+
 app.get("/", (req, res) => {
-  res.status(200).send({});
+  // getting the input quer params
+  let name = req.query.name;
+  console.log("user name:",name)
+  let city = req.query.city;
+  let role = req.query.role;
+  let GetDATA = {
+    name: name,
+    city: city,
+    role: role,
+  };
+  res.status(200).send({ status: 200, Input: GetDATA });
 });
 
 let PORT = 3004;
