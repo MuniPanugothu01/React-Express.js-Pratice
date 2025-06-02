7d79e4f5e53383dc663a12e51036df4d5a2b22a9
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-02 17:13:59 +0530

    data not showing code is correct

--- Server/Express/MongoConnect.js
+++ Server/Express/MongoConnect.js
@@ -2,14 +2,19 @@ const express = require("express");
 const app = express();
 const { mongoose, Schema } = require("mongoose");
 
+app.use(express.json());
+
 // connect mongodb to node js.
 mongoose
-  .connect("mongodb://localhost:27017/Table-Data")
+  .connect("mongodb://localhost:27017/Table-Data", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("mongodb is connected");
   })
   .catch((err) => {
-    console.log("mongodb is not connected here");
+    console.log("mongodb is not connected here",err);
   });
 // define the Schema what data type we mentioned in ur mongodb
 let UserSchema = new mongoose.Schema({
@@ -18,11 +23,18 @@ let UserSchema = new mongoose.Schema({
   role: String,
 });
 // to defined the model and get data from mongodb we can mention the table name it mean collection name
-let userModel = mongoose.model("user",UserSchema);
-// create the router 
-app.get('/getUser',(req,res)=>{
-    
-})
+let UserModel = mongoose.model("user", UserSchema);
+// create the router
+app.get("/getUser", (req, res) => {
+  UserModel.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('error in rectriving data')
+    });
+});
 
 let PORT = 3009;
 app.listen(PORT, () => {
