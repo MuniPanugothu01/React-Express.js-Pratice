e0918fa3c1d50675f7ed37ddda2ce3271d765f17
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-02 16:28:50 +0530

    installed the mongoose and while using mongoose to connect the mongodb url

--- Server/Express/MongoConnect.js
+++ Server/Express/MongoConnect.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const { mongoose } = require("mongoose");
+
+// connect mongodb to node js.
+mongoose
+  .connect("mongodb://localhost:27017/")
+  .then((res) => {
+    console.log(res, "mongodb is connected");
+  })
+  .catch((err) => {
+    console.log("mongodb is not connected here");
+  });
 
 let PORT = 3009;
 app.listen(PORT, () => {
