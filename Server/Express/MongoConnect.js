e13c6d37e189af12beee15d1e4274e66320df12f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-02 16:48:12 +0530

    defned the model and get the collection name in mongodb

--- Server/Express/MongoConnect.js
+++ Server/Express/MongoConnect.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
-const { mongoose } = require("mongoose");
+const { mongoose, Schema } = require("mongoose");
 
 // connect mongodb to node js.
 mongoose
-  .connect("mongodb://localhost:27017/")
+  .connect("mongodb://localhost:27017/Table-Data")
   .then(() => {
     console.log("mongodb is connected");
   })
   .catch((err) => {
     console.log("mongodb is not connected here");
   });
+// define the Schema what data type we mentioned in ur mongodb
+let UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  role: String,
+});
+// to defined the model and get data from mongodb we can mention the table name it mean collection name
+let userModel = mongoose.model("user",UserSchema);
+// create the router 
+app.get('/getUser',(req,res)=>{
+    
+})
 
 let PORT = 3009;
 app.listen(PORT, () => {
