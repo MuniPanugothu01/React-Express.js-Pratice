00fff38b39f8ed51eaa99872ea49a5687b0d8d6d
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-03 14:45:36 +0530

    data not showing in server

--- Server/Express/MongoConnect/index.js
+++ Server/Express/MongoConnect/index.js
@@ -4,11 +4,31 @@ const { DbConnect } = require("./Database");
 
 // import the file dotenv
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 const app = express();
 // function call
 DbConnect();
+
+// create the Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  role: String,
+});
+// model
+const UserModel = mongoose.model("Datas", userSchema);
+
+// routers
+app.get("/getData", async (req, res) => {
+  try {
+    let Data = await UserModel.find();
+    res.json(Data);
+  } catch (err) {
+    res.status(500).json({ message: "error occured fetching the data" });
+  }
+});
 
 let PORT = process.env.PORT;
 app.listen(PORT, () => {
