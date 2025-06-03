89caf7c9fcadf300441cbdd5c5828b53e6a1b137
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-03 14:24:24 +0530

    port also taken from dotenv file

--- Server/Express/MongoConnect/index.js
+++ Server/Express/MongoConnect/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 // import the database file
 const { DbConnect } = require("./Database");
+
+// import the file dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 // function call
 DbConnect();
 
-let PORT = 3010;
+let PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`port is connected ${PORT}`);
 });
