7d30427fdf97134b3cd892cfdd9ae09d810d9fdf
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-04 11:52:22 +0530

    getting data from mongodb database

--- Server/Express/CrudMongo/index.js
+++ Server/Express/CrudMongo/index.js
@@ -2,9 +2,26 @@ const express = require("express");
 const app = express();
 
 const dotenv = require("dotenv");
-const {DbConnect} = require("./db.js");
-DbConnect()
+const { DbConnect } = require("./db.js");
+const { mongoose } = require("mongoose");
+DbConnect();
 dotenv.config();
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  city: String,
+});
+const UserModel = mongoose.model("reels", UserSchema);
+app.get("/user", async (req, res) => {
+  try {
+    let DataFind = await UserModel.find();
+    res.json(DataFind);
+    console.log(DataFind, "data");
+  } catch (err) {
+    res.status(500).send({ message: "server error" });
+  }
+});
 
 let Port = process.env.PORT;
 app.listen(Port, () => {
