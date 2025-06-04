1d4e55fd535235e5f6eff6182231a03b9a0d1fa7
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-04 14:09:00 +0530

    PUT method is not working properly

--- Server/Express/CrudMongo/index.js
+++ Server/Express/CrudMongo/index.js
@@ -14,25 +14,28 @@ const UserSchema = new mongoose.Schema({
   city: String,
 });
 const UserModel = mongoose.model("reels", UserSchema);
-// Post the data to mongodb using of POST Method
+// Schema POST
 const SchemaPost = new mongoose.Schema({
   name: String,
   age: Number,
+  role: String,
   city: String,
 });
 const ModelPost = mongoose.model("users", SchemaPost);
-// router
+
+// POST
 app.post("/post", async (req, res) => {
   let Data = new ModelPost({
     name: req.body.name,
     age: req.body.age,
     role: req.body.role,
+    city: req.body.city,
   });
   await Data.save();
   console.log(req.body);
   res.status(200).send({ message: "data posted to body" });
 });
-
+// GET Method
 app.get("/user", async (req, res) => {
   try {
     let DataFind = await UserModel.find();
@@ -40,6 +43,30 @@ app.get("/user", async (req, res) => {
     console.log(DataFind, "data");
   } catch (err) {
     res.status(500).send({ message: "server error" });
+  }
+});
+
+// PUT method
+app.put("/post/:id", async (req, res) => {
+  try {
+    let UpdateData = ModelPost.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      overwrite: true,
+    });
+    res.send(UpdateData);
+  } catch (err) {
+    res.status(500).json({ message: "error in id" });
+  }
+});
+
+// DELETE Method
+
+app.delete("/post/:id", async (req, res) => {
+  try {
+    let DeletData = await ModelPost.findByIdAndUpdate(req.params.id);
+    res.send(DeletData);
+  } catch (err) {
+    res.status(500).send("Detele Error");
   }
 });
 
