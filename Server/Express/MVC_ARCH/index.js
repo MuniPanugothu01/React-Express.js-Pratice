2fc7b4e5acddc5721570d8f647f4e97f8390e317
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-09 12:11:26 +0530

    created the seperate folders as useing MVC arch controllers models routes

---
+++ Server/Express/MVC_ARCH/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+// dotenv file imppor
+const dotenv = require("dotenv");
+dotenv.config();
+// db.js file import
+const { DbConnect } = require("./db.js");
+DbConnect();
+
+let PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`port is connected ${PORT}`);
+});
