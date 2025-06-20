643140f66994c72ac0fd6ab20a1d9e347c390f7f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-20 15:19:51 +0530

    i took  the seperate app.use() handle route functions

--- Server/Namaste/index_multiHandle.js
+++ Server/Namaste/index_multiHandle.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const app = express();
 
-// multiple handle function
-app.use("/router", rH1, [rH2, rH3], rH4);
-app.use(
-  "/user",
-  rH1,
-  (req, res, next) => {
-    console.log("response1");
-    next();
-  },
+const rH1 = (req, res, next) => {
+  console.log("response1");
+  next();
+};
+const rH2 = (req, res, next) => {
+  console.log("response2");
+  next();
+};
+const rH3 = [
   (req, res, next) => {
     console.log("response2");
     next();
   },
-  [
-    (req, res, next) => {
-      console.log("response2");
-      next();
-    },
-    (req, res, next) => {
-      console.log("response3");
-      res.send("response3");
-    },
-  ]
-);
+  (req, res, next) => {
+    console.log("response3");
+    res.send("response3");
+  },
+];
+// multiple handle function
+app.use("/router", rH1, [rH2, rH3]);
 
 app.listen(3002, () => {
   console.log(`port is connected ${3002}!`);
