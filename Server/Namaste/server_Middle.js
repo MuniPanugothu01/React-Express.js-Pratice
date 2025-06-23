512509101037419696b013159ed07a0b73bf7087
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-23 06:57:51 +0530

    admin crenditentail is all perfect

--- Server/Namaste/server_Middle.js
+++ Server/Namaste/server_Middle.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const { adminAuth } = require('./middleWare/adminAuth');
+const express = require("express");
+const { adminAuth } = require("./middleWare/adminAuth");
 const app = express();
 
-app.use('/admin',adminAuth)
+app.use("/admin", adminAuth);
 
-app.get('/admin/home',(req,res)=>{
-    console.log()
-})
+app.get("/admin/home", (req, res) => {
+  res.status(200).send("home section");
+});
+app.post("/admin/about", (req, res) => {
+  res.status(201).send("about section");
+});
 
 let PORT = 3002;
-app.listen(PORT,()=>{
-    console.log(`port is connected ${PORT}`);    
-})
+app.listen(PORT, () => {
+  console.log(`port is connected ${PORT}`);
+});
