2fb97d771c85bbb592c8f6559c8251ed0fe6b0cc
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-07-02 06:03:40 +0530

    api validation

--- Server/Namaste/models/user.js
+++ Server/Namaste/models/user.js
@@ -1,59 +1,61 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 10,
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true, // if user can give space it remove the spaces
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    // enum : ["male", "female", "others"],
-    lowercase: true,
-    trim: true,
-    
-    validate(value) {
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("gender is not validate");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 10,
     },
-    
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true, // if user can give space it remove the spaces
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      // enum : ["male", "female", "others"],
+      lowercase: true,
+      trim: true,
+
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("gender is not validate");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_hybrid&w=740",
+    },
+    about: {
+      type: String,
+      default: "this is the defalut about of the user!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  photoUrl: {
-    type: String,
-    default:
-      "https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_hybrid&w=740",
-  },
-  about: {
-    type: String,
-    default: "this is the defalut about of the user!",
-  },
-  skills: {
-    type: [String],
-  },
-},{
-  timestamps:true
-});
+  {
+    timestamps: true,
+  }
+);
 
 const UserModel = mongoose.model("user", userSchema);
 module.exports = {
