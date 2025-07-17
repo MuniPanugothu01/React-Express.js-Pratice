55026aa33dd5e669958590ca102ea8a8f19f13f0
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-07-17 15:12:51 +0530

    authUser middle ware, while user can login then only he can get the data from server, other wise it don't give the me response,

--- Server/Namaste/middleWare/adminAuth.js
+++ Server/Namaste/middleWare/adminAuth.js
@@ -1,12 +1,32 @@
 // import the jwt
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+// import the UserModel
+const UserModel = require("../models/user.js");
 
-
-const userAuth = (req, res, next) => {
-// 
+const userAuth = async (req, res, next) => {
+  try {
+    // read the tokens from the req cookies
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid token!");
+    }
+    //verify the token and decode it
+    const DecodedData = await jwt.verify(token, "dev@tendirproject");
+    console.log("authUser DecodeData", DecodedData);
+    const { _id } = DecodedData;
+    // find the userId in mongoDB
+    const user = await UserModel.findById(_id);
+    if (!user) {
+      throw new Error("User is not found!");
+    }
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send({ status: 400, "Error: ": error.message });
+  }
 };
 
 module.exports = {
-
   userAuth,
 };
